ab8815048096582905db9be13babecd23e90fdea
Alicia de Wet <alicia@feeditback.com>
2024-10-27 10:57:08 +0000

    :construction: add first API call to get films, use types, add props to card component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
+import {ref} from 'vue'
+
+import type { Character, Film, FilmsResponse } from '@/types/filmTypes'
 
 import DetailsCard from '@/components/base/DetailsCard/DetailsCard.vue'
+
+const films = ref<Film[]>([])
+const character = ref<Character>()
+const showTryLater = ref<boolean>(false)
+
+const getFilms = async (): Promise<void> => {
+  const response = await fetch('https://swapi.dev/api/films')
+
+  if (response && response.status == 200) {
+    const filmsResponse: FilmsResponse = await response.json()
+
+    films.value = filmsResponse.results
+  } else {
+    showTryLater.value = true
+  }
+}
+
+getFilms()
 </script>
 
 <template>
@@ -12,12 +33,21 @@ import DetailsCard from '@/components/base/DetailsCard/DetailsCard.vue'
       <div class="left-block">
         Films
 
-        <details-card />
+        <details-card v-for="film in films" :key="film.episode_id" :film />
       </div>
       <div class="right-block">
         Characters
+
+        <details-card :character />
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
 
