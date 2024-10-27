d01754cd4071ffa56b463c024b26b21a4aca6d27
Alicia de Wet <alicia@feeditback.com>
2024-10-27 10:25:02 +0000

    :construction: setting up

---
+++ src/components/base/DetailsCard/DetailsCard.spec.ts
@@ -0,0 +1,12 @@
+import { describe, it, expect } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+
+import DetailsCard from '@/components/base/DetailsCard/DetailsCard.vue'
+
+describe('DetailsCard', () => {
+  it('renders', () => {
+    const wrapper = shallowMount(DetailsCard)
+
+    expect(wrapper.exists()).toBe(true)
+  })
+})
---
+++ src/components/base/DetailsCard/DetailsCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="details-card">
+  <h3>Card Title</h3>
+
+  <div>
+    <p>Director</p>
+    <p>Release</p>
+  </div>
+
+  <div>
+    <p>attribute 1</p>
+    <p>attribute 2</p>
+    <p>attribute 3</p>
+    <p>attribute 4</p>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.details-card {
+  border-radius: 5px;
+  border: 1px solid slategrey;
+  background: lightslategray;
+}
+</style>
