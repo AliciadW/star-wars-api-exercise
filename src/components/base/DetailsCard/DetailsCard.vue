34ccba587088e0290233d9c80861d0550d19ee80
Alicia de Wet <alicia@feeditback.com>
2024-10-27 10:57:28 +0000

    :construction: start working with received data

--- src/components/base/DetailsCard/DetailsCard.vue
+++ src/components/base/DetailsCard/DetailsCard.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-
+const props = defineProps(['film', 'character'])
 </script>
 
 <template>
 <div class="details-card">
-  <h3>Card Title</h3>
-
-  <div>
-    <p>Director</p>
-    <p>Release</p>
+  <div v-if="film">
+    <h3 class="details-card-title">{{ film.title }}</h3>
+    <div>
+      <p>{{ film.director }}</p>
+      <p>{{ film.release_date }}</p>
+    </div>
   </div>
 
-  <div>
+  <div v-else-if="character">
+    <h3 class="details-card-title">{{ character.name }}</h3>
     <p>attribute 1</p>
     <p>attribute 2</p>
     <p>attribute 3</p>
@@ -25,5 +27,7 @@
   border-radius: 5px;
   border: 1px solid slategrey;
   background: lightslategray;
+  margin-bottom: 5px;
+  padding: 5px;
 }
 </style>
